f202343f8ad644c48b1a0f4c25e8d11acc715278
demfnanda <fernanda_menezes72@hotmail.com>
2025-12-01 13:37:53 +0000

    Atualização do Banner 💡

---
+++ src/components/Banner.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+    <section class="banner">
+        <h1>Onde a paisagem vira 
+            <span>inspiração</span> .
+        </h1>
+        
+        <p>Exploque paisagens, cores e luzes que inspiram novas histórias através da fotografia.</p>
+        
+    </section>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
