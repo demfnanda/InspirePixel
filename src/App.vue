7d5ff3d5420a3136ff377a1aff0368bb66dbe68a
demfnanda <fernanda_menezes72@hotmail.com>
2025-12-01 13:03:18 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Header from './components/Header.vue';
+</script>
+
+<template>
+  <h1>Página principal</h1>
+  <Header/>
+</template>
+
+<style></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<script setup>
+import logo from "../assets/logo.png"
+// import { Icon } from "@iconify/vue"
+import pessoa from "../assets/pessoa.png"
+import busca from "../assets/busca.png"
+
+</script>
+
+<template>
+
+<header>
+    <img :src="logo" alt="">
+    <nav>
+        <li>
+            <a href="">
+               <img :src="busca" alt=""> <!-- NÃO ESQUECER DE COLOCAR OS : ANTES DO SRC-->
+            </a>
+        </li>
+        <li>
+            <a href="">Início</a>
+        </li>
+        <li>
+            <a href="">Galeria</a>
+        </li>
+        <li>
+            <a href="">Favoritos</a>
+        </li>
+        <li>
+            <a href="">
+                <img src="" alt="">
+            </a>
+        </li>
+    </nav>
+   
+</header>
+
+</template>
+
+<style scoped lang="scss">
+    header{
+        border: solid;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        flex-wrap: wrap;
+        margin: 2rem;
+    }
+</style>
