e589198a2f3beacc5fc2ca7b4798b23802f9f22c
demfnanda <fernanda_menezes72@hotmail.com>
2025-12-01 19:45:49 +0000

    Criação do componente Fotos 📸

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Header from './components/Header.vue';
 import Banner from "./components/Banner.vue";
+import Fotos from "./components/Fotos.vue";
 </script>
 
 <template>
-  <Header/>
-  <Banner/>
+  <Header />
+  <Banner />
+  <Fotos />
 </template>
 
 <style></style>
